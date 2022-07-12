d4a18d7dca88009733f7e70b81c12f72313545c7
fabiano.capobianchi <fabiano.capobianchi@agid.gov.it>
2022-07-12 10:47:58 +0200

    Passaggio alla versione 2.0

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ namespace fcapobianchi\HelloWorld;
 
 class Index
 {
-    public function greet($greet = "Hello World")
+    public function greet($greet = "Hello World V 2.0")
     {
         return $greet;
     }
